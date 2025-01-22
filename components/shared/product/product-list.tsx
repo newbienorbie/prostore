c95d2e3c20a8eac59f37ff63faca17840069f8a9
Norberta Sandrine <151909800+newbienorbie@users.noreply.github.com>
2025-01-22 13:21:01 +0800

    fixed for deployment

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,23 +1,21 @@
 import ProductCard from "./product-card";
 import { Product } from "@/types";
 
-const ProductList = ({
-  data,
-  title,
-  limit,
-}: {
+interface ProductListProps {
   data: Product[];
   title?: string;
   limit?: number;
-}) => {
+}
+
+export default function ProductList({ data, title, limit }: ProductListProps) {
   const limitedData = limit ? data.slice(0, limit) : data;
 
   return (
     <div className="my-10">
-      <h2 className="h2-bold mb-4">{title}</h2>
+      {title && <h2 className="h2-bold mb-4">{title}</h2>}
       {data.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {limitedData.map((product: Product) => (
+          {limitedData.map((product) => (
             <ProductCard key={product.slug} product={product} />
           ))}
         </div>
@@ -28,6 +26,4 @@ const ProductList = ({
       )}
     </div>
   );
-};
-
-export default ProductList;
+}
--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,10 @@
 import { z } from "zod";
 import { insertProductSchema } from "@/lib/validators";
+import { Decimal } from "@prisma/client/runtime/library";
 
 export type Product = z.infer<typeof insertProductSchema> & {
   id: string;
-  rating: string;
+  rating: Decimal;
+  numReviews: number;
   createdAt: Date;
 };
