df2ea8225c07a8f996c3e707158c679a6761eef6
Norberta Sandrine <151909800+newbienorbie@users.noreply.github.com>
2025-01-22 13:39:26 +0800

    fixed

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,35 +1,16 @@
-// lib/utils.ts
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { Decimal } from "@prisma/client/runtime/library";
-import { Product } from "@/types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-// convert prisma object into regular JS object while preserving Decimal types
+// Convert prisma object into a regular JS object
 export function convertToPlainObject<T>(value: T): T {
-  const plainObj = JSON.parse(JSON.stringify(value));
-
-  // If the value is an array, process each item
-  if (Array.isArray(plainObj)) {
-    return plainObj.map((item) => ({
-      ...item,
-      price: new Decimal(item.price),
-      rating: new Decimal(item.rating),
-    })) as T;
-  }
-
-  // If it's a single object
-  return {
-    ...plainObj,
-    price: new Decimal(plainObj.price),
-    rating: new Decimal(plainObj.rating),
-  } as T;
+  return JSON.parse(JSON.stringify(value));
 }
 
-// format number with decimal places for price
+// Format number with decimal places
 export function formatNumberWithDecimal(num: number): string {
   const [int, decimal] = num.toString().split(".");
   return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
