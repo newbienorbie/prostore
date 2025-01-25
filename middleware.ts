c36d604297496a88a0b5338c17033b093aa672d4
Norberta Sandrine <151909800+newbienorbie@users.noreply.github.com>
2025-01-25 23:56:36 +0800

    middleware reduce size

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,16 @@
-export { auth as middleware } from "@/auth";
+import { NextResponse } from "next/server";
+import { auth } from "@/auth";
+
+export async function middleware(request: Request) {
+  const session = await auth();
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/dashboard/:path*", "/admin/:path*"],
 };
